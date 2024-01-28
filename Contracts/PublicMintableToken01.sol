be16e21e2359f93bf449308f52f7aa0089be6a2d
Ben Token Finance <admin@bentoken.finance>
2024-01-28 19:20:34 +0800

    Update PublicMintableToken01.sol

--- Contracts/PublicMintableToken01.sol
+++ Contracts/PublicMintableToken01.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.4/contracts/token/ERC20/ERC20.sol";
 
 contract PublicMintableToken01 is ERC20 {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
