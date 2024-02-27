89c95055b8b8e69a4346076a2f29e8950b7545b3
Ben Token Finance <admin@bentoken.finance>
2024-02-27 12:50:42 +0800

    Update BlindBoxes.sol

--- Contracts/BlindBoxes.sol
+++ Contracts/BlindBoxes.sol
@@ -1,22 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.4/contracts/token/ERC20/IERC20.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.4/contracts/token/ERC20/utils/SafeERC20.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.4/contracts/token/ERC721/ERC721.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.4/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.4/contracts/access/Ownable.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.4/contracts/utils/Strings.sol"; 
 
-interface VrfGovIfc {
-    function verify (
-        uint256 blockHash,
-        uint256 rdm,
-        bytes calldata pi
-    ) external view returns (bool);
-}
-
-contract BlindBoxes is ERC721, ERC721Enumerable, Ownable {
+contract BlindBoxes is ERC721Enumerable, Ownable {
     using SafeERC20 for IERC20;
     using Strings for uint256;
        
@@ -55,15 +46,18 @@ contract BlindBoxes is ERC721, ERC721Enumerable, Ownable {
     uint256 public constant MaxSupply = 200;
     uint256 public constant Price = 99 * 1e18;
     uint256 public constant Reward = 8888 * 1e18;
+    address private constant VrfSigner = 0x18C51aa3d1F018814716eC2c7C41A20d4FAf023C;
+    string constant private PREFIX = "\x19Ethereum Signed Message:\n40";
 
     uint64 public claimCount = 0; 
-    uint64 public vrfBlock;
+    uint64 public randSeedBlock;
+    uint256 public randomSeedForClaim;
+    uint256 public openedBoxMask;
 
     event Minted(address indexed user, uint256 tokenId);
     event Opened(address indexed user, uint256 tokenId, uint256 cardType);
     event Claimed(address indexed user, uint256[] tokenIds);
 
-    mapping(uint256 => uint256) public getCardType;
     mapping(uint256 => bool) public isClaimed;
 
     constructor(string memory name_, string memory symbol_, string memory uri_) ERC721(name_, symbol_) {
@@ -72,20 +66,17 @@ contract BlindBoxes is ERC721, ERC721Enumerable, Ownable {
         counter = 1;
     }
 
-    function shuffledPosToCardType(uint i) internal pure returns (uint8) {
+    function getCardType(uint tokenId) public view returns (uint) {
+	if((openedBoxMask&(1<<tokenId))==0) return 0;
+	return _getCardType(tokenId);
+    }
+
+    function _getCardType(uint tokenId) internal view returns (uint) {
+        uint i = shuffle_pos(tokenId, MaxSupply);
         if(i < 14) return uint8(0xf&(0x44444333332211>>(i*4))); 
-        return uint8((i-14)/31 + 5);
+        return (i-14)/31 + 5;
     }
 
-    address private constant VrfgovAddress = 0x18C51aa3d1F018814716eC2c7C41A20d4FAf023C;
-    function overrideVrfBlock() external onlyOwner {
-        require(counter > MaxSupply, "Not all minted!");
-        require(randomSeedForClaim == 0, "Already set!");
-        require(vrfBlock > 0 && vrfBlock + 1000 < block.number, "Not Applicable!");
-
-        // In the rare cases when the backend bot failed to set randomSeedForClaim in time, this is maybe necessary.
-        vrfBlock = uint64(block.number);
-    }
 
     function mint(address to) public returns(uint256) {
         uint256 tokenId = counter;
@@ -98,7 +89,7 @@ contract BlindBoxes is ERC721, ERC721Enumerable, Ownable {
         emit Minted(to, tokenId);
 
         if (tokenId == MaxSupply) {
-            vrfBlock = uint64(block.number);
+            randSeedBlock = uint64(block.number);
         }
 
         return tokenId;
@@ -121,21 +112,40 @@ contract BlindBoxes is ERC721, ERC721Enumerable, Ownable {
         }
 
         if (tokenId + qty -1 == MaxSupply) {
-            vrfBlock = uint64(block.number);
+            randSeedBlock = uint64(block.number);
         }
 
         return tokenIds;
     }
 
-    uint256 public randomSeedForClaim;
+    function openBox(uint256 tokenId) public returns(uint256) {
+        require(randomSeedForClaim > 0, "Not ready!");
+        require(ownerOf(tokenId)==msg.sender, "Not owner!");
+        uint oldMask = openedBoxMask;
+        uint newMask = oldMask&(1<<tokenId);
+        require(oldMask!=newMask, "Already opened!");
+        openedBoxMask = newMask;
 
-    function setRandomSeedForClaim(uint256 rdm, bytes calldata pi) external {
-        require(vrfBlock > 0, "VRF block not ready!");
+        return _getCardType(tokenId);
+    }
+
+    function openBoxes(uint256[] calldata tokenIds) external returns(uint256[] memory) {
+        uint256[] memory cardTypes = new uint256[](tokenIds.length);
+
+        for (uint i=0; i<tokenIds.length; i++) {
+            cardTypes[i] = openBox(tokenIds[i]);
+        }
+
+        return cardTypes;
+    }
+
+    function setRandomSeedForClaim(uint256 rdm, uint8 v, bytes32 r, bytes32 s) external {
+        require(randSeedBlock > 0, "VRF block not ready!");
         require(randomSeedForClaim == 0, "Already set!");
 
-        bytes32 hash = blockhash(vrfBlock);
-        require (uint256(hash) > 0, "Invalid block hash!");
-        require(VrfGovIfc(VrfgovAddress).verify(uint256(hash), rdm, pi), "Invalid vrf!");
+        bytes32 hash = keccak256(abi.encodePacked(PREFIX, randSeedBlock, rdm));
+	    address signer = ecrecover(hash, v, r, s);
+        require(signer == VrfSigner, "Invalid Signer");
 
         randomSeedForClaim = rdm;
     }
@@ -166,32 +176,12 @@ contract BlindBoxes is ERC721, ERC721Enumerable, Ownable {
         return x;
     }
 
-    function openBox(uint256 tokenId) public returns(uint256) {
-        require(randomSeedForClaim > 0, "Not ready!");
-        require(ownerOf(tokenId)==msg.sender, "Not owner!");
-        require(getCardType[tokenId]==0, "Already opened!");
-
-        uint256 cardType = shuffledPosToCardType(shuffle_pos(tokenId, MaxSupply));
-        getCardType[tokenId] = cardType;
-        return cardType;
-    }
-
-    function openBoxes(uint256[] calldata tokenIds) external returns(uint256[] memory) {
-        uint256[] memory cardTypes = new uint256[](tokenIds.length);
-
-        for (uint i=0; i<tokenIds.length; i++) {
-            cardTypes[i] = openBox(tokenIds[i]);
-        }
-
-        return cardTypes;
-    }
-
     function claim(uint256[] calldata tokenIds) external {
         // tokenIds are already sorted by cardType in the frontend code
         require(tokenIds.length == 10, "Wrong cards!");
         for(uint i=0; i<10; i++) {
             require(ownerOf(tokenIds[i])==msg.sender, "Not owner!");
-            require(getCardType[tokenIds[i]]==i+1, "Wrong card type!");
+            require(_getCardType(tokenIds[i])==i+1, "Wrong card type!");
             require(!isClaimed[tokenIds[i]], "Already claimed!");
 
             isClaimed[tokenIds[i]] = true;
@@ -209,20 +199,4 @@ contract BlindBoxes is ERC721, ERC721Enumerable, Ownable {
         return 4;   // All claimed
     }
 
-    // The following functions are overrides required by Solidity.
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
 }
