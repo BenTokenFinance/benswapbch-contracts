0d866243f28b7e1f75392578b8d04d51579bc3c8
freelance <freelance@gmail.com>
2024-09-20 17:47:18 +0800

    update calcuatePower

--- Contracts/AntAgingPills.sol
+++ Contracts/AntAgingPills.sol
@@ -7,183 +7,151 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/proxy/utils/Initializable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/token/ERC721/ERC721.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "https://github.com/abdk-consulting/abdk-libraries-solidity/blob/master/ABDKMath64x64.sol";
 import "https://raw.githubusercontent.com/abdk-consulting/abdk-libraries-solidity/master/ABDKMathQuad.sol";
 
 
 interface BasicNft is IERC721,IERC721Enumerable {
     function mint(address to) external;
+    function batchMint(address to,uint256  number) external;
     function setTokenURL(string memory prefix_,string memory suffix_) external;
 }
 contract AntAgingPills is Initializable,Ownable{
-    
+    using ABDKMathQuad for bytes16;
     struct lockedInfo{
        uint256 lockedAmount;
        uint256 startTime;
        uint256 endTime;
-       uint8   exponent;
+       uint256 exponent;
+       uint256 withdrawnAmount;
+       bool exists;
     }
-    // private _data;
     address public  feeToken;
     address public  nftAddress;
-    mapping (uint256=>lockedInfo) lockedInfoByTokenId;
-
+    mapping (uint256=>lockedInfo) public lockInfoById;
     event lockAntAgingCreated(uint256 tokenId,address to,uint256 startTime,uint256 endTime,uint256 exponent,uint256 tokenAmount);
-
+    event lockAntAgingWithdrawal(uint256 tokenId,address user,uint256 withdrawnAmount,uint256 releasedTokens);
+    
+    //test tokenId
+    uint256 public tokenId;
     function initialize(address _feeToken) public initializer {
         feeToken=_feeToken;
+        tokenId=1;
     }
     
-    function createAntAging(address to,uint256 tokenAmount,uint256 endTime,uint8 exponent) public {
+    /*
+     exponent 幂传递*100的整数,例如0.25*100=25
+    */
+    function createAntAging(address to,uint256 tokenAmount,uint256 startTime,uint256 endTime,uint256 exponent) public {
        require(to!=address(0),"faild address");
        require(tokenAmount>0,"Can't be less than 0");
-       require(endTime>block.timestamp,"Can't be less than now");
-
-       IERC20 tokenContract=IERC20(feeToken);
-       tokenContract.transferFrom(msg.sender,address(this),tokenAmount);
-       // 发送代币
-       BasicNft nftContract=BasicNft(nftAddress);
-       nftContract.mint(to);
-       uint256 tokenId=nftContract.totalSupply();
-       lockedInfoByTokenId[tokenId]=lockedInfo(tokenAmount,block.timestamp,endTime,exponent);
-       emit lockAntAgingCreated(tokenId,to,block.timestamp,endTime,exponent,tokenAmount);
+       require(endTime>startTime,"Can't be less than now");
+       //  TO DO:
+       //   IERC20 tokenContract=IERC20(feeToken);
+       //   tokenContract.transferFrom(msg.sender,address(this),tokenAmount);
+       //   BasicNft nftContract=BasicNft(nftAddress);
+       //   nftContract.mint(to);
+       //   uint256 tokenId=nftContract.totalSupply();
+       //   lockInfoById[tokenId]=lockedInfo(tokenAmount,startTime,endTime,exponent,0);
+       lockInfoById[tokenId]=lockedInfo(tokenAmount,block.timestamp,block.timestamp+60,exponent,0,true);
+       tokenId+=1;
+       emit lockAntAgingCreated(tokenId,to,startTime,endTime,exponent,tokenAmount);
     }
-
-    function getReleasedTokens(uint256 tokenId) view external returns(uint256 releasedTokens) {
-           uint256 elapsedTime = block.timestamp - lockedInfoByTokenId[tokenId].startTime; // 已过时间
-           uint256 totalReleaseTime=lockedInfoByTokenId[tokenId].endTime-lockedInfoByTokenId[tokenId].startTime;
-           releasedTokens=calculatePower(lockedInfoByTokenId[tokenId].lockedAmount,elapsedTime,totalReleaseTime,lockedInfoByTokenId[tokenId].exponent/100,100);
+    
+    //查询tokenId对应的释放数量
+    function getReleasedTokens(uint256 tokenId) view public  returns(uint256 releasedTokens) {
+           require(lockInfoById[tokenId].exists,"The tokenId does not exist");
+           require(block.timestamp>lockInfoById[tokenId].startTime,"The token is not released");
+           uint256 elapsedTime = block.timestamp - lockInfoById[tokenId].startTime; // 已过时间
+           uint256 totalReleaseTime=lockInfoById[tokenId].endTime-lockInfoById[tokenId].startTime;// 总释放时间
+           //已过时间大于等于总释放时间，返回剩余所有locked；
+           if (elapsedTime >= totalReleaseTime) {
+               return lockInfoById[tokenId].lockedAmount;
+           }
+           releasedTokens=calculatePower(lockInfoById[tokenId].lockedAmount,elapsedTime,totalReleaseTime,lockInfoById[tokenId].exponent,100);
            return  releasedTokens;
     } 
 
-    // using ABDKMath64x64 for int128;
-    // function calculatePower(
-    //     uint256 totalTokens, 
-    //     uint256 elapsedTime,
-    //     uint256 totalTime,
-    //     uint256 exponentNumerator,
-    //     uint256 exponentDenominator
-    // ) public pure returns (uint256) {
-    //     // 确保 totalTime 不为零
-    //     require(totalTime > 0, "Total time must be greater than zero");
-    //     // 确保 elapsedTime 不大于 totalTime
-    //     require(elapsedTime <= totalTime, "Elapsed time cannot exceed total time");
-    //     // 将时间转换为定点数
-    //     int128 elapsedTimeFixed = ABDKMath64x64.fromUInt(elapsedTime);
-    //     int128 totalTimeFixed = ABDKMath64x64.fromUInt(totalTime);
-
-    //     // 计算时间比例
-    //     int128 ratio = elapsedTimeFixed.div(totalTimeFixed);
-
-    //     // 确保比例大于零
-    //     require(ratio > 0, "Ratio must be greater than zero");
-
-    //     // 将幂指数转换为定点数
-    //     int128 exponent = ABDKMath64x64.divu(exponentNumerator, exponentDenominator);
-
-    //     // 计算 ln(ratio)
-    //     int128 lnRatio = ratio.ln();
-
-    //     // 计算 exponent * ln(ratio)
-    //     int128 exponentLnRatio = exponent.mul(lnRatio);
-    //     int128 powerResult = exponentLnRatio.exp();
-
-    //     // 将 totalTokens 转换为定点数
-    //     // int128 totalTokensFixed = ABDKMath64x64.fromUInt(totalTokens);
-    //     int128 totalTokensFixed = ABDKMath64x64.div(
-    //      ABDKMath64x64.fromUInt(totalTokens),
-    //      ABDKMath64x64.fromUInt(1e18)
-    //     );
-
-    //     // 计算 releasedTokens = totalTokens * powerResult
-    //     int128 releasedTokensFixed = totalTokensFixed.mul(powerResult);
-    //     uint256 scalingFactor = 10 ** uint256(18);
-    //     uint256 releasedTokens = releasedTokensFixed.mulu(scalingFactor);
-    //     return releasedTokens;
-    // }
-
-
-    using ABDKMathQuad for bytes16;
-
+    // 计算幂释放token数量
     function calculatePower(
-        uint256 totalTokens, 
-        uint256 elapsedTime,
-        uint256 totalTime,
-        uint256 exponentNumerator,
-        uint256 exponentDenominator
-    ) public pure returns (uint256) {
-        // 确保 totalTime 不为零
-        require(totalTime > 0, "Total time must be greater than zero");
-        // 确保 elapsedTime 不大于 totalTime
-        require(elapsedTime <= totalTime, "Elapsed time cannot exceed total time");
-        // 确保 exponentDenominator 不为零
-        require(exponentDenominator > 0, "Exponent denominator must be greater than zero");
+      uint256 totalTokens, 
+      uint256 elapsedTime,
+      uint256 totalTime,
+      uint256 exponentNumerator,
+      uint256 exponentDenominator
+   ) internal  pure returns (uint256) {
+      // 确保 totalTime 不为零
+      require(totalTime > 0, "Total time must be greater than zero");
+      // 确保 elapsedTime 不大于 totalTime
+      require(elapsedTime <= totalTime, "Elapsed time cannot exceed total time");
+      // 确保 exponentDenominator 不为零
+      require(exponentDenominator > 0, "Exponent denominator must be greater than zero");
 
-        // 计算时间比例 ratio = elapsedTime / totalTime
-        bytes16 ratio = ABDKMathQuad.div(
-            ABDKMathQuad.fromUInt(elapsedTime),
-            ABDKMathQuad.fromUInt(totalTime)
-        );
+      // 计算时间比例 ratio = elapsedTime / totalTime
+      bytes16 ratio = ABDKMathQuad.div(
+         ABDKMathQuad.fromUInt(elapsedTime),
+         ABDKMathQuad.fromUInt(totalTime)
+      );
 
-        // 确保比例大于零
-        require(
-            ABDKMathQuad.cmp(ratio, ABDKMathQuad.fromUInt(0)) > 0,
-            "Ratio must be greater than zero"
-        );
+      // 确保比例大于零
+      require(
+         ABDKMathQuad.cmp(ratio, ABDKMathQuad.fromUInt(0)) > 0,
+         "Ratio must be greater than zero"
+      );
 
-        // 计算 exponent = exponentNumerator / exponentDenominator
-        bytes16 exponent = ABDKMathQuad.div(
-            ABDKMathQuad.fromUInt(exponentNumerator),
-            ABDKMathQuad.fromUInt(exponentDenominator)
-        );
+      // 计算 exponent = exponentNumerator / exponentDenominator
+      bytes16 exponent = ABDKMathQuad.div(
+         ABDKMathQuad.fromUInt(exponentNumerator),
+         ABDKMathQuad.fromUInt(exponentDenominator)
+      );
 
-        // 计算 powerResult = exp(exponent * ln(ratio))
-        bytes16 powerResult = ABDKMathQuad.exp(
-            ABDKMathQuad.mul(
-                exponent,
-                ABDKMathQuad.ln(ratio)
-            )
-        );
+      // 计算 powerResult = exp(exponent * ln(ratio))
+      bytes16 powerResult = ABDKMathQuad.exp(
+         ABDKMathQuad.mul(
+               exponent,
+               ABDKMathQuad.ln(ratio)
+         )
+      );
 
-        // 计算 releasedTokensQuad = (totalTokens / 1e18) * powerResult
-        bytes16 releasedTokensQuad = ABDKMathQuad.mul(
-            ABDKMathQuad.div(
-                ABDKMathQuad.fromUInt(totalTokens),
-                ABDKMathQuad.fromUInt(1e18)
-            ),
-            powerResult
-        );
+      // 直接计算 releasedTokensQuad = totalTokens * powerResult
+      bytes16 releasedTokensQuad = ABDKMathQuad.mul(
+         ABDKMathQuad.fromUInt(totalTokens),
+         powerResult
+      );
 
-        // 将结果乘以 1e18，恢复代币的精度
-        releasedTokensQuad = ABDKMathQuad.mul(
-            releasedTokensQuad,
-            ABDKMathQuad.fromUInt(1e18)
-        );
+      // 将结果转换回 uint256
+      uint256 releasedTokens = ABDKMathQuad.toUInt(releasedTokensQuad);
 
-        // 将结果转换回 uint256
-        uint256 releasedTokens = ABDKMathQuad.toUInt(releasedTokensQuad);
+      return releasedTokens;
+   }
 
-        return releasedTokens;
-    }
 
+    // 提取当前奖励
     function withdrawReleasedTokens(uint256 tokenId) external {
-    //    uint256 elapsedTime = block.timestamp - lockedInfoByTokenId[tokenId].startTime; // 已过时间
-    //    uint256 totalReleaseTime=lockedInfoByTokenId[tokenId].endTime-lockedInfoByTokenId[tokenId].startTime;
-    //     // 确保时间不超过总释放时间
-    //    if (elapsedTime > totalReleaseTime) {
-    //         elapsedTime = lockedInfoByTokenId[tokenId].totalReleaseTime;
-    //    }
-    //    uint256 releasedAmount = lockedInfoByTokenId[tokenId].lockedAmount * ((elapsedTime/totalReleaseTime) ** lockedInfoByTokenId[tokenId].exponent);
-
-       // 计算释放的代币数量: 总锁定数量 * (已过时间 / 总释放时间) ^ 幂
-       //    uint256 releasedAmount = totalLockedAmount * (elapsedTime ** exponent) / (totalReleaseTime ** exponent);
-       //    return 0;
-       //    {总锁定数量} * Math.pow({已经过时间}/{总释放时间}, {幂});  
+      require(lockInfoById[tokenId].exists,"The tokenId does not exist");
+      // TO DO:
+      // BasicNft nftContract=BasicNft(nftAddress);
+      // require(nftContract.ownerOf(tokenId)==msg.sender,"Not the tokenId owner");
+      require(lockInfoById[tokenId].lockedAmount>lockInfoById[tokenId].withdrawnAmount,"The token has been extracted.");
+      uint256 releasedTokens=getReleasedTokens(tokenId);
+      require(releasedTokens>0,"Invalid token count");
+      uint256 amount=releasedTokens-lockInfoById[tokenId].withdrawnAmount;
+      require(amount>0,"Invalid token amount");
+      lockInfoById[tokenId].withdrawnAmount+=amount;
+      emit lockAntAgingWithdrawal(tokenId,msg.sender,amount,releasedTokens);
+      // TO DO:
+      // IERC20 tokenContract=IERC20(feeToken);
+      // require(amount>0,"Invalid token amount");
+      // tokenContract.transferFrom(address(this),msg.sender,releasedTokens);
     }
 
     function setTokenURL(string memory prefix,string memory suffix) external onlyOwner{
        BasicNft nftContract=BasicNft(nftAddress);
        nftContract.setTokenURL(prefix,suffix);
+    }
+
+    function batchMint(address to,uint256 number) external onlyOwner{
+       BasicNft nftContract=BasicNft(nftAddress);
+       nftContract.batchMint(to,number);
     }
 
 }
