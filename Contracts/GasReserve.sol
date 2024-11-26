0beb063010b17080fff3c956730c9231aa03552c
freelance <freelance@gamil.com>
2024-11-26 23:01:03 +0800

    update gasReserve

--- Contracts/GasReserve.sol
+++ Contracts/GasReserve.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.4;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
 contract GasReserve is Ownable{
+    event GasTransferred(address indexed recipient, uint256 amount);
     constructor() {
     }
     function transferGas(address payable recipient, uint256 amount) external onlyOwner {
         require(amount <= address(this).balance, "Insufficient balance in the contract");
         recipient.transfer(amount);
+        emit GasTransferred(recipient, amount);
     }
     receive() external payable {}
 
